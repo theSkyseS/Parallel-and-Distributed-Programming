965bee5e5c7ac182dce6dd7e1d5b2aaf4623d563
theSkyseS <--add>
2021-12-25 11:42:22 +0600

    refactor: removed Usages of TextArrayWritable updated pom.xml dependencies to be able to launch program in the local mode moved idea's project file

--- lab-7/src/main/java/Main.java
+++ lab-7/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
@@ -6,6 +7,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.File;
 
 public class Main extends Configured implements Tool {
 
@@ -15,21 +19,27 @@ public class Main extends Configured implements Tool {
     }
 
     public int run(String[] args) throws Exception {
-        if (args.length != 2) {
+        /*if (args.length != 2) {
             System.err.printf("Usage: %s [generic options] <input> <output>\n",
                     getClass().getSimpleName());
             ToolRunner.printGenericCommandUsage(System.err);
             return -1;
-        }
+        }*/
 
-        Job job = new Job();
+        FileUtils.deleteDirectory(new File("hadoopOut"));
+
+        BasicConfigurator.configure();
+
+        Job job = Job.getInstance();
         job.setJarByClass(Main.class);
         job.setJobName("Class Counter");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path("spring-framework-main/spring-core/src"));
+        FileOutputFormat.setOutputPath(job, new Path("hadoopOut"));
+
+        FileInputFormat.setInputDirRecursive(job, true);
 
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(TextArrayWritable.class);
+        job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setMapperClass(ClassCountMapper.class);
